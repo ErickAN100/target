9b9f5d23778eb8342d966692c52c0eff4b9d2f97
ErickAN100 <erickav72@gmail.com>
2025-01-19 18:32:55 -0300

    Update numero1.cpp

--- numero1.cpp
+++ numero1.cpp
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void main(){
-	int INDICE = 13;
-	int SOMA = 0;
-	int K = 0;
+int main() {
+    int INDICE = 13, SOMA = 0, K = 0;
 
-	while( K < INDICE)
-		{
-			K = K + 1;
-			SOMA = SOMA + K;
-		}
-		
-	printf("%d",SOMA);
-				
+    // Laço enquanto (while)
+    while (K < INDICE) {
+        K = K + 1;
+        SOMA = SOMA + K;
+    }
+
+    // Imprime o valor final de SOMA
+    printf("O valor de SOMA é: %d\n", SOMA);
+
+    return 0;
 }
